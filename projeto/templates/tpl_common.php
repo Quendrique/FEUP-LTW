87d1c63eed4a826209e2c64eb0cc61fd195a6911
luisdiogo98 <luisdiogo98@users.noreply.github.com>
2018-11-05 10:12:54 +0000

    added first pages

---
+++ projeto/templates/tpl_common.php
@@ -0,0 +1,23 @@
+<?php function draw_header() { ?>
+  <!DOCTYPE html>
+  <html>
+
+    <head>
+      <meta charset="utf-8">
+      <link rel="stylesheet" href="../css/style.css">
+      <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" crossorigin="anonymous">
+    </head>
+
+    <body>
+
+      <header>
+        <h1><a href="newpage.php"><i class="fas fa-plus-circle"></i> New Page</a></h1>
+        <h1><a href="channels.php"><i class="fas fa-hashtag"></i> Channels</a></h1>
+        <h1><a href="upload.php"><i class="fas fa-upload"></i> Upload</a></h1>
+      </header>    
+<?php } ?>
+
+<?php function draw_footer() {  ?>
+  </body>
+</html>
+<?php } ?>
