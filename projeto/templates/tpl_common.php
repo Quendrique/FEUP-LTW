bb03bc3853ca452fad7c38a1fa49fc4f16e8ff1b
Beatriz Mendes <bea.smendes98@gmail.com>
2018-11-05 23:10:23 +0000

    some css styling

--- projeto/templates/tpl_common.php
+++ projeto/templates/tpl_common.php
@@ -9,12 +9,21 @@
     </head>
 
     <body>
+      <div class = "navBar">
 
-      <header>
-        <h1><a href="newpage.php"><i class="fas fa-plus-circle"></i> New Page</a></h1>
-        <h1><a href="channels.php"><i class="fas fa-hashtag"></i> Channels</a></h1>
-        <h1><a href="upload.php"><i class="fas fa-upload"></i> Upload</a></h1>
-      </header>    
+      <img src= "../img/logo.png" height="40" width="40"/>
+        <nav class="tabs">
+          <ul>
+            <li><a href="profile.php"><i class="fas fa-bars"></i> </a></li>
+            <li><a href="upload.php"><i class="fas fa-upload"></i> Upload</a></li>
+            <li><a href="channels.php"><i class="fas fa-hashtag"></i> Channels</a></li>
+            <li><a href="newpage.php"><i class="fas fa-plus-circle"></i> New In</a></li>
+        </ul>
+
+        
+      </div>
+      </nav> 
+</div> 
 <?php } ?>
 
 <?php function draw_footer() {  ?>
