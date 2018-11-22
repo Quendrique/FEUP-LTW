671d8e57afe3a6c050682ad73a4a2b1a103b268a
luisdiogo98 <luisdiogo98@users.noreply.github.com>
2018-11-22 14:56:17 +0000

    Added login box

--- projeto/pages/mainpage.php
+++ projeto/pages/mainpage.php
@@ -1,5 +1,7 @@
 <?php
-    include_once('../templates/tpl_common.php');
+    include_once('templates/tpl_common.php');
+    include_once('templates/tpl_account.php');
     draw_header();
+    draw_sidebar_login();
     draw_footer();
 ?>
--- projeto/templates/tpl_common.php
+++ projeto/templates/tpl_common.php
@@ -4,26 +4,24 @@
 
     <head>
       <meta charset="utf-8">
+      <title>Site Name</title>
       <link rel="stylesheet" href="../css/style.css">
       <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" crossorigin="anonymous">
     </head>
 
     <body>
       <div class = "navBar">
-
-      <img src= "../img/logo.png" height="40" width="40"/>
-        <nav class="tabs">
-          <ul>
-            <li><a href="profile.php"><i class="fas fa-bars"></i> </a></li>
-            <li><a href="upload.php"><i class="fas fa-upload"></i> Upload</a></li>
-            <li><a href="channels.php"><i class="fas fa-hashtag"></i> Channels</a></li>
-            <li><a href="newpage.php"><i class="fas fa-plus-circle"></i> New In</a></li>
-        </ul>
-
-        
-      </div>
-      </nav> 
-</div> 
+        <img src= "../img/logo.png" height="40" width="40"/>
+          <nav class="tabs">
+            <ul>
+              <li><a href="profile.php"><i class="fas fa-bars"></i> </a></li>
+              <li><a href="upload.php"><i class="fas fa-upload"></i> Upload</a></li>
+              <li><a href="channels.php"><i class="fas fa-hashtag"></i> Channels</a></li>
+              <li><a href="newpage.php"><i class="fas fa-plus-circle"></i> New In</a></li>
+            </ul>
+          </nav>
+      </div> 
+ 
 <?php } ?>
 
 <?php function draw_footer() {  ?>
