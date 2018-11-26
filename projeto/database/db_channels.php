dabaf888e5b2bc6263a0ce7e8b39e28876a3548d
unknown <mariana.costa214@gmail.com>
2018-11-26 10:08:15 +0000

    started channels page

---
+++ projeto/database/db_channels.php
@@ -0,0 +1,14 @@
+<?php
+  include('db_connection.php');
+
+  /**
+   * Returns the list of channels
+   */
+  function getChannels() {
+    global $db;
+    $stmt = $db->prepare('SELECT * FROM channels');
+    $stmt->execute(array());
+    return $stmt->fetchAll(); 
+  }
+
+?>
