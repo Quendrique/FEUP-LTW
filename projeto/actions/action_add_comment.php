d6137e398a236bbab67e0081ad31dc0d097c9d08
unknown <mariana.costa214@gmail.com>
2018-12-06 23:26:01 +0000

    user can now add comments to a story

---
+++ projeto/actions/action_add_comment.php
@@ -0,0 +1,24 @@
+<?php
+
+    include_once('../includes/incl_session.php');
+    include_once('../database/db_channels.php');
+    include_once('../database/db_stories.php');
+
+    $user = $_POST['user'];
+    $story = $_POST['story'];
+    $comment = $_POST['comment'];
+
+    if(isset($_SERVER['HTTP_REFERER'])) {
+      $prev_page = $_SERVER['HTTP_REFERER'];
+    } else {
+      $prev_page = "../pages/story_page.php?story_id=$story";
+    }
+
+    //try {
+      addComment($user, $story, $comment);
+      header("Location: ".$prev_page);
+    //} catch (PDOException $e) {
+      //$_SESSION['messages'][] = array('type' => 'error', 'content' => "Unable to post comment");
+      //die(header("Location: ".$prev_page));
+    //}
+?>
