8b65ccbee12ca1e6995d8ff5098cc78b8f17c1a2
unknown <mariana.costa214@gmail.com>
2018-12-11 18:43:58 +0000

    voting in sorted stories now working

--- projeto/scripts/vote_story.js
+++ projeto/scripts/vote_story.js
@@ -1,47 +1,48 @@
-let upvotes = document.querySelectorAll('article#story section#upvote button');
-let downvotes = document.querySelectorAll('article#story section#downvote button');
-upvotes.forEach((upvote) => upvote.addEventListener('click', voteStoryClicked)); 
-downvotes.forEach((downvote) => downvote.addEventListener('click', voteStoryClicked)); 
-
-
+let voteStory = document.querySelector('body');
+voteStory.addEventListener('click', voteStoryClicked)
 
 function voteStoryClicked(event) {
-  let info = event.currentTarget;
-  let user = info.getAttribute('user');
-  let story = info.getAttribute('story');
-  let action = info.getAttribute('action');
-  let oldUpVotes = document.querySelector('article#story section#upvote[data-storyid=' + CSS.escape(story) + '] #numUpvotes').innerHTML;
-  let oldDownVotes = document.querySelector('article#story section#downvote[data-storyid=' + CSS.escape(story) + '] #numDownvotes').innerHTML;;
-  let newUpVotes = document.querySelector('article#story section#upvote[data-storyid=' + CSS.escape(story) + '] #numUpvotes');
-  let newDownVotes = document.querySelector('article#story section#downvote[data-storyid=' + CSS.escape(story) + '] #numDownvotes');
 
+  console.log(event.target.closest('article#story button'));
+
+  let info = event.target.closest('article#story button');
+
+  if (info) {
+    let user = info.getAttribute('user');
+    let story = info.getAttribute('story');
+    let action = info.getAttribute('action');
+    let oldUpVotes = document.querySelector('article#story section#upvote[data-storyid=' + CSS.escape(story) + '] #numUpvotes').innerHTML;
+    let oldDownVotes = document.querySelector('article#story section#downvote[data-storyid=' + CSS.escape(story) + '] #numDownvotes').innerHTML;;
+    let newUpVotes = document.querySelector('article#story section#upvote[data-storyid=' + CSS.escape(story) + '] #numUpvotes');
+    let newDownVotes = document.querySelector('article#story section#downvote[data-storyid=' + CSS.escape(story) + '] #numDownvotes');
+    
+    let request = new XMLHttpRequest();
+    request.open("POST", "../api/api_vote_story.php", true);
+    request.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+    request.addEventListener("load", function () {
+      let updatedStory = JSON.parse(this.responseText);
   
-  let request = new XMLHttpRequest();
-  request.open("POST", "../api/api_vote_story.php", true);
-  request.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
-  request.addEventListener("load", function () {
-    let updatedStory = JSON.parse(this.responseText);
-
-    newUpVotes.innerHTML = updatedStory.upvotes;
-    newDownVotes.innerHTML = updatedStory.downvotes;
-
-    //update styles
-    if(updatedStory.upvotes>oldUpVotes){
-      let downButton =  document.querySelector('article#story section#downvote[data-storyid=' + CSS.escape(story) + '] #votedownBtn');
-      styleButtons(info,newUpVotes,newDownVotes,downButton,"rgb(131, 193, 233)"); 
-
-    }else if(updatedStory.downvotes>oldDownVotes){
-      let upButton = document.querySelector('article#story section#upvote[data-storyid=' + CSS.escape(story) + '] #voteupBtn')
-      styleButtons(info,newDownVotes,newUpVotes,upButton,"#A46BE5"); 
-
-    }else{
-      newDownVotes.style.color = "#373843";
-      info.style.color = "#373843";
-      newUpVotes.style.color = "#373843";
-    }
-
-  });
-  request.send(encodeForAjax({user: user, story: story, action: action}));
+      newUpVotes.innerHTML = updatedStory.upvotes;
+      newDownVotes.innerHTML = updatedStory.downvotes;
+  
+      //update styles
+      if(updatedStory.upvotes>oldUpVotes){
+        let downButton =  document.querySelector('article#story section#downvote[data-storyid=' + CSS.escape(story) + '] #votedownBtn');
+        styleButtons(info,newUpVotes,newDownVotes,downButton,"rgb(131, 193, 233)"); 
+  
+      }else if(updatedStory.downvotes>oldDownVotes){
+        let upButton = document.querySelector('article#story section#upvote[data-storyid=' + CSS.escape(story) + '] #voteupBtn')
+        styleButtons(info,newDownVotes,newUpVotes,upButton,"#A46BE5"); 
+  
+      }else{
+        newDownVotes.style.color = "#373843";
+        info.style.color = "#373843";
+        newUpVotes.style.color = "#373843";
+      }
+  
+    });
+    request.send(encodeForAjax({user: user, story: story, action: action}));
+  }
 }
 
 function encodeForAjax(data) {
